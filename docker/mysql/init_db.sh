e73e1827dde0ea9e4e3de9ab849f48f986c65ff0
HULI <doddahulugappa@gmail.com>
2023-03-11 19:57:52 +0400

    docker-compose.yml updated - referred mysql image directly

--- docker/mysql/init_db.sh
+++ docker/mysql/init_db.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 mysql -u root --password=""  << EOF
+USE mysql
+UPDATE user set Host='%' where User='root';
 USE ${MYSQL_DATABASE};
 GRANT ALL PRIVILEGES ON *.* TO root@localhost IDENTIFIED BY '' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON  test_${MYSQL_DATABASE}.* TO '${MYSQL_USER}';
