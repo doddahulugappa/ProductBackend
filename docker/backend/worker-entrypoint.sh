ca0ad1f6ed29bfcd89f0a75dbd219826406c614a
HULI <doddahulugappa@gmail.com>
2023-03-13 00:19:20 +0400

    docker-compose.yml and worker-entrypoint.sh updated

--- docker/backend/worker-entrypoint.sh
+++ docker/backend/worker-entrypoint.sh
@@ -7,6 +7,8 @@ done
 
 # run celery worker & beat
 #celery -A backend worker --loglevel=info --concurrency 1 -E
+nohup docker exec -it api_container celery -A product_api worker --loglevel=info --concurrency 1 -E > /dev/null &
 
 #celery -A backend beat --loglevel=info --scheduler=django_celery_beat.schedulers.DatabaseScheduler
+nohup docker exec -it api_container celery -A product_api beat --loglevel=info > /dev/null &
 
