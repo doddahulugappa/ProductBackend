18973f25ae427fbd13f7d505a77d616670e24c8c
HULI <doddahulugappa@gmail.com>
2023-03-12 15:55:30 +0400

    product name & category name made unique

--- product_api/products/models.py
+++ product_api/products/models.py
@@ -7,14 +7,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     brand = models.CharField(max_length=50, null=True)
     description = models.CharField(max_length=200, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
